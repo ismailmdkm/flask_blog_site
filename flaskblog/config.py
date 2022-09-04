1e8e9cd7bb5020da470a16fef88d8cbd545f824b
Mohamed Ismail <ismailmdkm@gmail.com>
2022-09-04 15:13:52 +0400

    Blueprints and Configurations - restructure files

---
+++ flaskblog/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config:
+    SECRET_KEY = 'e49e40b1ecb9bddf401db4ca38197c62'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,11 +1,9 @@
-from ast import Sub
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
-
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[
@@ -57,13 +55,6 @@ class UpdateAccountForm(FlaskForm):
             if user:
                 raise ValidationError(
                     "Email already taken. Please choose a different one")
-
-
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
-
 
 class RequestResetForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
